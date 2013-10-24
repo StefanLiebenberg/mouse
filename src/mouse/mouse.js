a2deffd1186cf03eb12979881c4d85ad4c80ffe3
Stefan Liebenberg <siga.fredo@gmail.com>
2013-10-24 22:05:01 +0200

    adding goog and new binary

--- src/mouse/mouse.js
+++ src/mouse/mouse.js
@@ -1,1 +1,11 @@
 goog.provide('mouse');
+goog.require('mouse.query.Query');
+
+/**
+ * @param {string} queryString
+ * @return {Array.<Element>} elements
+ */
+mouse.search = function(queryString) {
+  var query = new mouse.query.Query(queryString);
+  return query.all();
+};
